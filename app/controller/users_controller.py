dfad3d1f1f4360a4da99a99543acc2b0856df2d7
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2022-07-24 10:58:57 -0500

    feat: add crud users controller

---
+++ app/controller/users_controller.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from ..models.models import Login, User
+from ..helpers import helper
+from ..database import users_db
+
+
+def validateLogin(user_: Login) -> bool:
+    user = helper.format_login(user_)
+    helper.validate_Login(user)
+    return users_db.validateLogin("email", user)
+
+
+def create(user_: User) -> User:
+    user = helper.format_user(user_)
+    helper.validate_user(user)
+    return users_db.create(user)
+
+
+def update(user_: User) -> User:
+    user_ = helper.format_name(user_)
+    helper.validate_user(user_)
+    return users_db.update(user_)
+
+
+def delete(contact: User) -> User:
+    return users_db.delete(contact)
+
+
+def lists() -> List[User]:
+    return users_db.list_all()
+
+
+def details(contact: User) -> User:
+    return users_db.detail(contact)
